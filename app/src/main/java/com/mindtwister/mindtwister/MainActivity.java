ed52202768f9e2d64ef4c774ff6bae5c366db7b6
UtkarshBehre <utkarsh.virtual@gmail.com>
2016-07-06 04:05:58 +0530

    initial commit

---
+++ app/src/main/java/com/mindtwister/mindtwister/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mindtwister.mindtwister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void quitOnClick(View view) {
+        finish();
+        System.exit(0);
+    }
+}
