8f03480ce63c0531e3dcb6954f2ed736fb9bef01
UtkarshBehre <utkarsh.virtual@gmail.com>
2016-07-08 05:33:56 +0530

    add game1 memory matrix , inception sound for testing sound

---
+++ app/src/main/java/com/mindtwister/mindtwister/Game1MemoryMatrixActivity.java
@@ -0,0 +1,16 @@
+package com.mindtwister.mindtwister;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Utkarsh on 08-07-2016.
+ */
+public class Game1MemoryMatrixActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game1_memory_matrix);
+    }
+}
