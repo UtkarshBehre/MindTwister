bd9cf4658068dcf0109919cbafece7eaa71d40aa
UtkarshBehre <utkarsh.virtual@gmail.com>
2016-07-18 08:21:58 +0530

    added SessionManager class and respective login changes

--- app/src/main/java/com/mindtwister/mindtwister/LoginActivity.java
+++ app/src/main/java/com/mindtwister/mindtwister/LoginActivity.java
@@ -1,19 +1,20 @@
 package com.mindtwister.mindtwister;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.*;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     DBHandler db;
+    SessionManager session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         db = new DBHandler(this);
+        session = new SessionManager(this);
     }
 
     public void register(View v) {
@@ -23,21 +24,22 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View v) throws NullPointerException {
-        EditText editText2 = (EditText) findViewById(R.id.etUsername);
-        String username = editText2.getText().toString();
 
-        EditText editText3 = (EditText) findViewById(R.id.etPassword);
-        String password = editText3.getText().toString();
+        //get user name input
+        EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        String username = etUsername.getText().toString();
 
-        //  RegisterClass rc = new RegisterClass();
+        //get password input
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        String password = etPassword.getText().toString();
 
-        //   rc.setUser_nickname(username);
-        //  rc.setUser_password(password);
+        //retrieve userinfo object from database
         RegisterClass rc = db.selectuser(username, password);
-        //  Toast.makeText(getApplicationContext(),rc.getUser_nickname(), Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, MainActivity.class);
-        // intent.putExtra("username",rc.getUser_nickname());
-        startActivity(intent);
+        if (rc != null) {
+            session.createLoginSession(rc.getUser_name(), rc.getUser_nickname(), rc.getUser_email());
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
         finish();
 
 
