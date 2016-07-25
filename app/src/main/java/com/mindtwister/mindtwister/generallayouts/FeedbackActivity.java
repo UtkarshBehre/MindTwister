a08915ddce2c235c6909a15580ec5172f041c266
UtkarshBehre <utkarsh.virtual@gmail.com>
2016-07-25 17:44:30 +0530

    added intent in feedback form to open email

--- app/src/main/java/com/mindtwister/mindtwister/generallayouts/FeedbackActivity.java
+++ app/src/main/java/com/mindtwister/mindtwister/generallayouts/FeedbackActivity.java
@@ -1,8 +1,12 @@
 package com.mindtwister.mindtwister.generallayouts;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.mindtwister.mindtwister.R;
 
@@ -14,6 +18,17 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+    }
+
+    public void feedbackSubmitOnClick(View view) {
+        TextView feedbackText = (TextView) findViewById(R.id.feedbackText);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Mind Twister Game feedback");
+        intent.putExtra(Intent.EXTRA_TEXT, feedbackText.getText().toString());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
 
